a96c7b5afa07e185c28e807e03e303411816289c
Dan Costello <dan.costello2@gmail.com>
2020-08-05 06:48:19 -0500

    Basic log viewer

--- README.txt
+++ README.txt
@@ -1,8 +1,9 @@
 todo:
 -add other log tracks
 -import real data from LAS
--add tops
+-add tops 
+    -tops are visible
+    -need to import from JSON
 -add input from user:
-    -start depth
-    -scale/zoom level
-    
+    -start depth DONE
+    -scale/zoom level DONE
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Calculate fake log
 var depth = [];
 var density = [];
 var gammaRay = [];
@@ -7,6 +8,7 @@ for (var i = 0; i < 2500; i++) {
   gammaRay.push(Math.random() * 100);
 }
 
+//set up tracks
 var trace1 = {
   x: gammaRay,
   y: depth,
@@ -21,34 +23,87 @@ var trace2 = {
 };
 var data = [trace1, trace2];
 
+//Adjust log width based on number of tracks
+document.getElementById('myDiv').style.width = data.length * 150;
+console.log('test12')
+//track layout
 var layout = {
   grid: {
     rows: 1,
     columns: 2,
   },
-  width: 400,
+  width: data.length * 150,
   dragmode: 'pan',
   showlegend: false,
   yaxis: {
-    //autorange: 'reversed',
     range: [50, 0],
-    //autorange: 'reversed'
+    title: {
+      text: 'Depth',
+    },
   },
   xaxis: {
     side: 'top',
     fixedrange: true,
     title: {
       text: 'GR',
-      x: -20
+      x: -20,
     },
   },
   xaxis2: {
     side: 'top',
     fixedrange: true,
     title: {
-        text: 'Density'
-    }
+      text: 'Density',
+    },
   },
+  margin: {
+    l: 30,
+    r: 50,
+    t: 50,
+    b: 30,
+  },
+  shapes: [
+    {
+      type: 'line',
+      xref: 'paper',
+      yref: 'y',
+      x0: 0,
+      y0: 10,
+      x1: 1,
+      y1: 10,
+      line: {
+        color: 'rgb(50,171,96)',
+        width: 3,
+      },
+    },
+  ],
+  annotations: [
+    {
+      xref: 'paper',
+      yref: 'y',
+      x: 1.12,
+      xanchor: 'right',
+      y: 10,
+      //yanchor: 'bottom',
+      text: 'Top1',
+      showarrow: false,
+    },
+  ],
+  plot_bgcolor: '#eeeeee',
+  paper_bgcolor: '#eeeeee',
 };
-
 Plotly.newPlot('myDiv', data, layout);
+
+function refreshView() {
+  var viewSettings = document.getElementById('ViewSettings');
+  var newStart = parseInt(viewSettings.elements[0].value);
+  var newZoom = parseInt(viewSettings.elements[1].value);
+  if (isNaN(newStart) || isNaN(newZoom) || newStart < 0 || newZoom < 0) {
+    alert('Input values need to be positive numbers.');
+    return;
+  }
+  var update = {
+    'yaxis.range': [newStart + newZoom, newStart],
+  };
+  Plotly.relayout('myDiv', update);
+}
