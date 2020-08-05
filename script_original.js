32f10bfb7de3d9948f312d9b131a6390adb47244
Dan Costello <dan.costello2@gmail.com>
2020-08-05 14:00:25 -0500

    Adding modularity

--- script_original.js
+++ script_original.js
@@ -1,138 +1,116 @@
-//GENERATE FAKE LOG
-var syntheticLog = (function () {
-  var depth = [];
-  var density = [];
-  var gammaRay = [];
-  for (var i = 0; i < 2500; i++) {
-    depth.push(i);
-    density.push(Math.random() / 2 + 2.4);
-    gammaRay.push(Math.random() * 100);
+//Calculate fake log
+
+var depth = [];
+var density = [];
+var gammaRay = [];
+for (var i = 0; i < 2500; i++) {
+  depth.push(i);
+  density.push(Math.random() / 2 + 2.4);
+  gammaRay.push(Math.random() * 100);
+}
+
+var well_string = wellio.loadLAS("00/01-01-073-05W5/0.LAS")
+las2json(las)
+
+//set up tracks
+var trace1 = {
+  x: gammaRay,
+  y: depth,
+  xaxis: 'x2',
+  type: 'scatter',
+};
+
+var trace2 = {
+  x: density,
+  y: depth,
+  type: 'line',
+};
+var data = [trace1, trace2];
+
+//Adjust log width based on number of tracks
+document.getElementById('myDiv').style.width = data.length * 150;
+
+//track layout
+var layout = {
+  grid: {
+    rows: 1,
+    columns: 2,
+  },
+  width: data.length * 150,
+  dragmode: 'pan',
+  showlegend: false,
+  yaxis: {
+    range: [50, 0],
+    title: {
+      text: 'Depth',
+    },
+  },
+  xaxis: {
+    side: 'top',
+    fixedrange: true,
+    title: {
+      text: 'GR',
+      x: -20,
+    },
+  },
+  xaxis2: {
+    side: 'top',
+    fixedrange: true,
+    title: {
+      text: 'Density',
+    },
+  },
+  margin: {
+    l: 70,
+    r: 30,
+    t: 80,
+    b: 30,
+  },
+  shapes: [
+    {
+      type: 'line',
+      xref: 'paper',
+      yref: 'y',
+      x0: 0,
+      y0: 10,
+      x1: 1,
+      y1: 10,
+      line: {
+        color: 'rgb(50,171,96)',
+        width: 3,
+      },
+    },
+  ],
+  annotations: [
+    {
+      xref: 'paper',
+      yref: 'y',
+      x: 1.12,
+      xanchor: 'right',
+      y: 10,
+      //yanchor: 'bottom',
+      text: 'Top1',
+      showarrow: false,
+    },
+  ],
+  plot_bgcolor: '#eeeeee',
+  paper_bgcolor: '#eeeeee',
+};
+Plotly.newPlot('myDiv', data, layout, { displayModeBar: false });
+
+function refreshView() {
+  var viewSettings = document.getElementById('ViewSettings');
+  var newStart = parseInt(viewSettings.elements[0].value);
+  var newZoom = parseInt(viewSettings.elements[1].value);
+  if (isNaN(newStart) || isNaN(newZoom) || newStart < 0 || newZoom < 0) {
+    alert('Input values need to be positive numbers.');
+    return;
   }
-  return {
-    Log: function () {
-      return {
-        depth: depth,
-        density: density,
-        gammaRay: gammaRay,
-      };
-    },
+  var update = {
+    'yaxis.range': [newStart + newZoom, newStart],
   };
-})();
-
-// GENERATE REAL LOG
-
-
-//PLOTLY GENERATION
-var setupPlotly = (function () {
-  //set up tracks
-  var trace1 = {
-    x: syntheticLog.Log().density,
-    y: syntheticLog.Log().depth,
-    xaxis: 'x2',
-    type: 'line',
-  };
-
-  var trace2 = {
-    x: syntheticLog.Log().gammaRay,
-    y: syntheticLog.Log().depth,
-    type: 'line',
-  };
-  var data = [trace1, trace2];
-
-  //track layout
-  var layout = {
-    grid: {
-      rows: 1,
-      columns: 2,
-    },
-    width: data.length * 150,
-    dragmode: 'pan',
-    showlegend: false,
-    yaxis: {
-      range: [50, 0],
-      title: {
-        text: 'Depth',
-      },
-    },
-    xaxis: {
-      side: 'top',
-      fixedrange: true,
-      title: {
-        text: 'GR',
-        x: -20,
-      },
-    },
-    xaxis2: {
-      side: 'top',
-      fixedrange: true,
-      title: {
-        text: 'Density',
-      },
-    },
-    margin: {
-      l: 70,
-      r: 30,
-      t: 80,
-      b: 30,
-    },
-    shapes: [
-      {
-        type: 'line',
-        xref: 'paper',
-        yref: 'y',
-        x0: 0,
-        y0: 10,
-        x1: 1,
-        y1: 10,
-        line: {
-          color: 'rgb(50,171,96)',
-          width: 3,
-        },
-      },
-    ],
-    annotations: [
-      {
-        xref: 'paper',
-        yref: 'y',
-        x: 1.12,
-        xanchor: 'right',
-        y: 10,
-        //yanchor: 'bottom',
-        text: 'Top1',
-        showarrow: false,
-      },
-    ],
-    plot_bgcolor: '#eeeeee',
-    paper_bgcolor: '#eeeeee',
-  };
-  Plotly.newPlot('myDiv', data, layout, { displayModeBar: false });
-
-  return {data:data}
-})();
-
-//UI CONTROLLER
-var UIController = (function () {
-  //Adjust log width based on number of tracks
-  document.getElementById('myDiv').style.width = setupPlotly.data.length * 150;
-
-  function refreshView() {
-    var viewSettings = document.getElementById('ViewSettings');
-    var newStart = parseInt(viewSettings.elements[0].value);
-    var newZoom = parseInt(viewSettings.elements[1].value);
-    if (isNaN(newStart) || isNaN(newZoom) || newStart < 0 || newZoom < 0) {
-      alert('Input values need to be positive numbers.');
-      return;
-    }
-    var update = {
-      'yaxis.range': [newStart + newZoom, newStart],
-    };
-    Plotly.relayout('myDiv', update);
-  }
-
-  return {
-    refreshView
-  }
-})();
+  Plotly.relayout('myDiv', update);
+}
 
 ////  Function that takes a single LAS text file representing a single well and returns an object variable in JSON format for that well.
 function las2json(onelas) {
@@ -256,7 +234,7 @@ function las2json(onelas) {
   console.log('onelas = ', onelas);
   var split1 = onelas.split('~');
   console.log('split1 = ', split1);
-  console.log(split1.length);
+  console.log(split1.length)
   var vers_str = '';
   var well_info_str = '';
   var curve_info_str = '';
